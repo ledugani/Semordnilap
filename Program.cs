e98d18b2b79896155bbf4bca19688aa8de2f8017
Tom Dugan <thomas@aadwebb.com>
2019-01-08 19:56:35 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semordnilap
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter the word you wish to check:");
+			Console.Write(">");
+			var x = Console.ReadLine();
+			Console.WriteLine(x);
+		}
+	}
+}
